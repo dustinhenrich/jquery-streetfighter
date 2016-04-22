1983aee6ce26d54bc4d94001dc0da390e03b07bf
Dustin Henrich <dustin.henrich@gmail.com>
2016-04-21 20:29:51 -0500

    Project Complete
    Project complete, though with very specific (but unknown) workflows,
    both Ryu Divs will appear vertically stacked on the page.

--- js/app.js
+++ js/app.js
@@ -4,42 +4,49 @@ $(document).ready(function() {
     $('.ryu-ready').show();
   })
   .mouseleave(function() {
-  	$('.ryu-ready').hide();
-  	$('.ryu-still').show();
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
   })
   .mousedown(function() {
-  	playHadouken();
-  	$('.ryu-ready').hide();
-  	$('.ryu-throwing').show();
-  	$('.hadouken').finish().show().animate(
-  		{'left':'1020px'},
-  		500,
-  		function() {
-			$(this).hide();
-			$(this).css('left','555px');
-  		});
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-cool').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show().animate(
+      {'left':'1020px'},
+      500,
+      function() {
+      $(this).hide();
+      $(this).css('left','555px');
+      });
   })
   .mouseup(function(){
-  	$('.ryu-throwing').hide();
-  	$('.ryu-ready').show();
+    $('.ryu-still').hide();
+    $('.ryu-cool').hide();
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
   })
 
   $('body').keydown(function(event) {
-  	if (event.which == 88 ) {
-  		$('.ryu-ready').hide();
-  		$('.ryu-still').hide();
-  		$('.ryu-cool').show();
-  	}
+    if (event.which == 88 ) {
+      $('.ryu-ready').hide();
+      $('.ryu-still').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-cool').show();
+    }
   })
 
   $('body').keyup(function() {
-  		$('.ryu-still').show();
-  		$('.ryu-cool').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-cool').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-still').show();
+      
   });
 });
 
 function playHadouken() {
-	$('#hadouken-sound')[0].volume = 0.5;
-	$('#hadouken-sound')[0].load();
-	$('#hadouken-sound')[0].play();
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
 }
